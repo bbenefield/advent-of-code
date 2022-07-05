d3b4602711fad92ad8712cc72fe996ebc9f6e0c4
Brett Benefield <bbenefield@gmail.com>
2022-07-04 22:17:59 -0400

    Initial commit

--- 2015/day3/day3.py
+++ 2015/day3/day3.py
@@ -3,31 +3,31 @@ import numpy as np
 with open('2015/day3/input.txt', 'r') as fin:
     data = fin.readline()
 
-# idx_x = 0
-# idx_y = 0
+idx_x = 0
+idx_y = 0
 
-# grid_list = list(data)
-# grid_count = len(grid_list) // 2
-# grid_arr = np.zeros((grid_count, grid_count))
-# grid_arr[idx_x][idx_y] = 1
+grid_list = list(data)
+grid_count = len(grid_list) // 2
+grid_arr = np.zeros((grid_count, grid_count))
+grid_arr[idx_x][idx_y] = 1
 
-# for char in grid_list:
-#     if char == '^':
-#         idx_y += 1
-#         grid_arr[idx_x][idx_y] += 1
-#     elif char == 'v':
-#         idx_y -= 1
-#         grid_arr[idx_x][idx_y] += 1
-#     elif char == '<':
-#         idx_x -= 1
-#         grid_arr[idx_x][idx_y] += 1
-#     elif char == '>':
-#         idx_x += 1
-#         grid_arr[idx_x][idx_y] += 1
-#     else:
-#         continue
+for char in grid_list:
+    if char == '^':
+        idx_y += 1
+        grid_arr[idx_x][idx_y] += 1
+    elif char == 'v':
+        idx_y -= 1
+        grid_arr[idx_x][idx_y] += 1
+    elif char == '<':
+        idx_x -= 1
+        grid_arr[idx_x][idx_y] += 1
+    elif char == '>':
+        idx_x += 1
+        grid_arr[idx_x][idx_y] += 1
+    else:
+        continue
     
-# print(f'Deliveries made: {np.count_nonzero(grid_arr)}')
+print(f'Deliveries made: {np.count_nonzero(grid_arr)}')
 
 class Delivery(object):
     
@@ -63,12 +63,7 @@ for idx, char in enumerate(data):
     else:
         robot.move(char)
 
-santa_dict = santa.get_dict()
-robot_dict = robot.get_dict()
-
-final_dict = {**santa_dict, **robot_dict}
+final_dict = {**santa.get_dict(), **robot.get_dict()}
       
 print(f'Deliveries made: {len(final_dict)}')
-
-#2361
         
